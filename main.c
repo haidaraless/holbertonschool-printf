14b9748f0fdf3e37e88ff77a04086f13f16f3649
Miad008 <10694@holbertonstudents.com>
2025-03-29 03:21:46 +0000

    main.h

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main(void)
 {
 	int len = _printf("Hello %s, your grade is %c and %% good!\n", "Alice", 'A');
-	_printf("Printed %d characters.\n", len);
-	return 0;
+_printf("Printed %d characters.\n", len);
+return 0;
 }
