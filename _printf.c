4c6133276e71cbeaba32df265f2f543b6eede6c3
Miad008 <10694@holbertonstudents.com>
2025-04-02 07:07:02 +0000

    Handle basic format specifiers: %c, %s, %, %d, %i

--- _printf.c
+++ _printf.c
@@ -1,78 +1,10 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-#include <limits.h>
 
 /**
- * print_number - Prints an integer
- * @n: The integer to print
- *
- * Return: Number of characters printed
- */
-int print_number(int n)
-{
-unsigned int num;
-char buffer[12];
-int i = 0, count = 0;
-
-if (n < 0)
-{
-count += write(1, "-", 1);
-num = -((unsigned int)n);
-}
-else
-{
-num = n;
-}
-
-if (num == 0)
-return (write(1, "0", 1));
-
-while (num)
-{
-buffer[i++] = (num % 10) + '0';
-num /= 10;
-}
-while (i--)
-count += write(1, &buffer[i], 1);
-
-return (count);
-}
-
-/**
- * handle_specifier - Handles format specifiers
- * @format: The format specifier
- * @args: Argument list
- *
- * Return: Number of characters printed
- */
-int handle_specifier(const char *format, va_list args)
-{
-char *s;
-int count = 0;
-
-if (*format == 'c')
-return (write(1, &(char){va_arg(args, int)}, 1));
-else if (*format == 's')
-{
-s = va_arg(args, char *);
-if (!s)
-s = "(null)";
-while (*s)
-count += write(1, s++, 1);
-return (count);
-}
-else if (*format == 'd' || *format == 'i')
-return (print_number(va_arg(args, int)));
-else if (*format == '%')
-return (write(1, "%", 1));
-
-return (write(1, "%", 1) + write(1, format, 1));
-}
-
-/**
- * _printf - Produces output according to a format
- * @format: The format string
+ * _printf - Custom printf implementation
+ * @format: Format string
  *
  * Return: Number of characters printed
  */
@@ -91,7 +23,7 @@ while (*format)
 if (*format == '%' && *(format + 1))
 {
 format++;
-count += handle_specifier(format, args);
+count += route_specifier(format, args);
 }
 else
 {
