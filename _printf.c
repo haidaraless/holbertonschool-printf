f28ee18eb859bf6d8737ac60ee68e24739462208
Miad008 <10694@holbertonstudents.com>
2025-04-02 06:33:40 +0000

    Handle basic format specifiers: %c, %s, %, %d, %i

--- _printf.c
+++ _printf.c
@@ -1,18 +1,55 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to print
+ *
+ * Return: Number of characters printed
+ */
+int print_number(int n)
+{
+unsigned int num;
+char buffer[12];
+int i = 0, count = 0;
+
+if (n < 0)
+{
+count += write(1, "-", 1);
+num = -((unsigned int)n);
+}
+else
+{
+num = n;
+}
+
+if (num == 0)
+return (write(1, "0", 1));
+
+while (num)
+{
+buffer[i++] = (num % 10) + '0';
+num /= 10;
+}
+while (i--)
+count += write(1, &buffer[i], 1);
+
+return (count);
+}
 
 /**
  * handle_specifier - Handles format specifiers
- * @format: Pointer to current specifier
+ * @format: The format specifier
  * @args: Argument list
  *
  * Return: Number of characters printed
  */
 int handle_specifier(const char *format, va_list args)
 {
-char *s, buffer[12];
-int count = 0, i, n, neg;
+char *s;
+int count = 0;
 
 if (*format == 'c')
 return (write(1, &(char){va_arg(args, int)}, 1));
@@ -26,22 +63,10 @@ count += write(1, s++, 1);
 return (count);
 }
 else if (*format == 'd' || *format == 'i')
-{
-n = va_arg(args, int), neg = 0;
-if (n == 0)
-return (write(1, "0", 1));
-if (n < 0)
-neg = 1, n = -n;
-for (i = 0; n; i++, n /= 10)
-buffer[i] = (n % 10) + '0';
-if (neg)
-buffer[i++] = '-';
-while (i--)
-count += write(1, &buffer[i], 1);
-return (count);
-}
+return (print_number(va_arg(args, int)));
 else if (*format == '%')
 return (write(1, "%", 1));
+
 return (write(1, "%", 1) + write(1, format, 1));
 }
 
@@ -58,7 +83,9 @@ int count = 0;
 
 if (!format)
 return (-1);
+
 va_start(args, format);
+
 while (*format)
 {
 if (*format == '%' && *(format + 1))
@@ -67,9 +94,12 @@ format++;
 count += handle_specifier(format, args);
 }
 else
+{
 count += write(1, format, 1);
+}
 format++;
 }
+
 va_end(args);
 return (count);
 }
