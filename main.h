6b8573e0c2d7b22e40d5d91aa912429501cbe535
Miad008 <10694@holbertonstudents.com>
2025-04-02 07:39:47 +0000

    Add specifier routing for format handling

--- main.h
+++ main.h
@@ -7,5 +7,20 @@
  * Desc: A Header file contains all prototype functions
  */
 
-	int _printf(const char *format, ...);
-#endif
+#include <stdarg.h>
+
+/* Core printf */
+int _printf(const char *format, ...);
+
+/* Specifier routing */
+int route_specifier(const char *spec, va_list args);
+
+/* Number printing */
+int print_signed(long n);
+int print_unsigned(unsigned int n);
+
+/* Base printing */
+int print_hex(unsigned int n, int upper);
+int print_octal(unsigned int n);
+
+#endif /* MAIN_H */
--- spec_router.c
+++ spec_router.c
@@ -3,40 +3,81 @@
 #include <stdarg.h>
 
 /**
- * route_specifier - Routes to the correct printer
+ * handle_char - Handles %c specifier
+ * @args: va_list
+ * Return: Printed characters
+ */
+int handle_char(va_list args)
+{
+char c = va_arg(args, int);
+return (write(1, &c, 1));
+}
+
+/**
+ * handle_string - Handles %s specifier
+ * @args: va_list
+ * Return: Printed characters
+ */
+int handle_string(va_list args)
+{
+char *s = va_arg(args, char *);
+int count = 0;
+
+if (!s)
+s = "(null)";
+while (*s)
+count += write(1, s++, 1);
+return (count);
+}
+
+/**
+ * handle_percent - Handles %% specifier
+ * Return: Printed characters
+ */
+int handle_percent(void)
+{
+return (write(1, "%", 1));
+}
+
+/**
+ * route_specifier - Routes to the correct handler
  * @spec: Format specifier
- * @args: Arguments
- *
+ * @args: va_list
  * Return: Printed characters
  */
 int route_specifier(const char *spec, va_list args)
 {
 if (*spec == 'c')
-return (write(1, &(char){va_arg(args, int)}, 1));
+return (handle_char(args));
 else if (*spec == 's')
-{
-char *str = va_arg(args, char *);
-int count = 0;
-
-if (!str)
-str = "(null)";
-while (*str)
-count += write(1, str++, 1);
-return (count);
-}
+return (handle_string(args));
 else if (*spec == '%')
-return (write(1, "%", 1));
+return (handle_percent());
 else if (*spec == 'd' || *spec == 'i')
-return (print_signed(va_arg(args, int)));
+{
+int n = va_arg(args, int);
+return (print_signed int(n));
+}
 else if (*spec == 'u')
-return (print_unsigned(va_arg(args, unsigned int)));
+{
+int n = va_arg(args, int);
+return (print_unsigned int((unsigned int)n));
+}
 else if (*spec == 'x')
-return (print_hex(va_arg(args, unsigned int), 0));
+{
+int n = va_arg(args, int);
+return (print_hex((unsigned int)n, 0));
+}
 else if (*spec == 'X')
-return (print_hex(va_arg(args, unsigned int), 1));
+{
+int n = va_arg(args, int);
+return (print_hex((unsigned int)n, 1));
+}
 else if (*spec == 'o')
-return (print_octal(va_arg(args, unsigned int)));
-
+{
+int n = va_arg(args, int);
+return (print_octal((unsigned int)n));
+}
 write(1, "%", 1);
 return (write(1, spec, 1) + 1);
 }
